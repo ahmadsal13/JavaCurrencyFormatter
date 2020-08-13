a9fadde9e0eeaed6c612587cc7d60c0a9c8d1bba
Ahmad Salem <ahmadsalem5454@gmail.com>
2020-08-12 20:02:13 -0500

    first commit

---
+++ src/NumberFormat.java
@@ -0,0 +1,12 @@
+import java.util.Locale;
+
+public class NumberFormat {
+
+	public static void getCurrenyInstance(Locale us) {
+	}
+
+	public static Object getCurrencyInstance(Locale china) {
+		return null;
+	}
+
+}
